685f95bf220e6a185d06fbe47295b6a105a1e525
letusfly85 <letusfly95@gmail.com>
2015-05-14 21:07:03 +0900

    first commit

---
+++ lib/itamae/plugin/resource/sqlserver_user.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/sqlserver_user/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module SqlserverUser
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/itamae/plugin/resource/sqlserver_user_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Itamae::Plugin::Resource::SqlserverUser do
+  it 'has a version number' do
+    expect(Itamae::Plugin::Resource::SqlserverUser::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
